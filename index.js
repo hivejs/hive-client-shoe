4420e6b153963db7015e18c722ba4be490845fd9
Marcel Klehr <mklehr@gmx.net>
2015-08-05 16:08:59 +0200

    Callback after authentication

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var reconnect = require('shoe-bin')
   , dataplex = require('dataplex')
 
-module.exports = function(baseURL, access_token) {
+module.exports = function(baseURL, access_token, cb) {
   var plex = dataplex()
     , stream = shoe(baseURL+'/socket')
   stream.pipe(plex).pipe(stream)
 
   var authStream = plex.open('/authenticate')
   authStream.write(access_token)
+  authStream.once('data', function(chunk){
+    try {
+      cb(null, JSON.parse(chunk))
+    }catch(e){
+      cb(new Error('Authentication failed'))
+    }
+  })
   return plex
 }
