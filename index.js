5cf856eccc8602f9548400180e3666d9d3ca40c6
Marcel Klehr <mklehr@gmx.net>
2015-12-28 22:45:22 +0100

    Adjust to new /authenticate protocol + switch from cb-style to promise

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 var shoe = require('shoe-bin')
   , dataplex = require('dataplex')
 
-module.exports = function(baseURL, access_token, cb) {
+module.exports = function(baseURL, access_token) {
   var plex = dataplex()
     , stream = shoe(baseURL+'/socket')
   stream.pipe(plex).pipe(stream)
 
   var authStream = plex.open('/authenticate')
-  authStream.once('data', function(chunk){
-    try {
-      cb(null, JSON.parse(chunk))
-    }catch(e){
-      cb(new Error('Authentication failed'))
-    }
+  return new Promise(function(resolve, reject) {
+    authStream.once('data', function(chunk){
+      try {
+        var auth = JSON.parse(chunk).authenticated
+        if(auth) {
+          resolve(plex)
+        }else {
+          reject(new Error('Authentication failed'))
+        }
+      }catch(e){
+        reject(new Error('Authentication failed due to a connection problem'))
+      }
+    })
+    authStream.write(access_token)
   })
-  authStream.write(access_token)
-  return plex
 }
